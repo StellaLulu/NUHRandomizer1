ee9897b6ab4ffe0ee63c6465cd5aa0b6839cafa0
yueliang zhang <yueliangzhang@ymail.com>
2019-02-20 15:38:46 +0800

    randomizer

---
+++ NUHRandomizer/NUHRandomizer/Randomize.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Randomize.aspx.cs" Inherits="NUHRandomizer.Models.Randomize" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br />
+            <br />
+            <asp:Label ID="lblPatientID" runat="server" Text="Patient ID:"></asp:Label>
+            <asp:TextBox ID="txtPatientID" runat="server"></asp:TextBox>
+            <br />
+            <br />
+        </div>
+        <asp:Button ID="btnRandomize" runat="server" OnClick="ButtonRandomize_Click" Text="Randomize" />
+    </form>
+</body>
+</html>
---
+++ NUHRandomizer/NUHRandomizer/Randomize.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace NUHRandomizer.Models
+{
+    public partial class Randomize : System.Web.UI.Page
+    {
+        NUHRandomizerEntities context = new NUHRandomizerEntities();
+        Random random = new Random();
+        int lowHighNumber;
+        int stopContinueNumber;
+        int strata;
+        string ars;
+        string trialID;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lowHighNumber = random.Next(0, 10);
+            stopContinueNumber = random.Next(0, 10);
+        }
+
+        protected void ButtonRandomize_Click(object sender, EventArgs e)
+        {
+            if(lowHighNumber < 5)
+            {
+                strata = 1;
+            }
+            else
+            {
+                strata = 2;
+            }
+            if(stopContinueNumber < 5)
+            {
+                ars = "s";
+            }
+            else
+            {
+                ars = "c";
+            }
+            List<Patient> patients = context.Patients.ToList();
+            int count = context.Patients.Count();
+            
+        }
+    }
+}
